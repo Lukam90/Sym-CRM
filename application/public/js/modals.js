cb04dcf26bedf1ae7ed2ca6157b26c8e2593d30e
Lukam90 <lucien.hamm@gmail.com>
2022-02-24 18:00:58 +0100

    MAJ Modale Suppression V3

--- application/public/js/modals.js
+++ application/public/js/modals.js
@@ -5,3 +5,20 @@ const closeModal = (id, event = null) => {
 
     if (event)  event.preventDefault();
 };
+
+const deleteModal = (button) => {
+    const dataSet = button.dataset;
+
+    const id = dataSet.id;
+    const name = dataSet.name;
+    const type = dataSet.type;
+    const title = dataSet.title;
+    const token = dataSet.token;
+
+    $("#deleteName").innerHTML = name;
+    $("#deleteTitle").innerHTML = title;
+    $("#deleteForm").action = `/${type}/delete/${id}`;
+    $("#deleteToken").value = token;
+
+    openModal("#deleteModal");
+}
